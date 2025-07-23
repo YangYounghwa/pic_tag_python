ee2047844eb23a4d37d98c775b954cde4a78c030
김동찬 <dongchan@gmail.com>
2025-07-23 17:37:39 +0900

    feature/django index.html, home url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("",home)
 ]
