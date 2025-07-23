59caf2df12bf6ffbc799c314e2b8239a163e8a58
YangYounghwa <yh9369@gmail.com>
2025-07-23 16:10:05 +0900

    minor change

--- pic_tag/main.py
+++ pic_tag/main.py
@@ -1,9 +1,7 @@
-from pic_tag.dashboard import app
 from pic_tag import camera_manger
 
 
 def main():
-    app.run(debug=True,host='0,0.0.0', port=5000)
     camera_manger.start_all_cameras()
 
 if __name__ == "__main__":
