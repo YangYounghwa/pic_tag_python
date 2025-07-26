9fda150021f780e15579f3dfcf9d4c937a7b1b1c
YangYounghwa <51239748+YangYounghwa@users.noreply.github.com>
2025-07-27 01:42:33 +0900

    Html, css, js 분리
    static에 있는 파일 수정 후
    python manage.py collectstatic
    하면 알아서 staticfiles에 옮겨줍니다.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+
+
 # Application definition
+
+
+
+
+
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -67,6 +74,16 @@ TEMPLATES = [
         },
     },
 ]
+
+# static files setup
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+
 
 WSGI_APPLICATION = "backend.wsgi.application"
 ASGI_APPLICATION = "backend.asgi.application"
