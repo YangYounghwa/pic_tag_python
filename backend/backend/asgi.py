6acf94b1bafb29270ab6c55f730fc8a40514d212
YangYounghwa <51239748+YangYounghwa@users.noreply.github.com>
2025-07-27 18:56:40 +0900

    처음부터 다시 만들었습니다. mjpeg으로 main 화면 보내고 thumbnail은 ws열어서 opencv로 한장한장 이미지 보내고 있습니다.

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,44 +8,16 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-import sys
-# from channels.auth import AuthMiddlewareStack
-# from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
-
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-
-import monitoring.routing
-import camera.routing
-
-
-# print("[DEBUG] camera.routing module path:", camera.routing.__file__)
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             camera.routing.websocket_urlpatterns
-#         )
-#     ),
-# })
-
-
-import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import camera.routing  # must be importable
+import camera_stream.routing
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
-print("🔥 asgi.py is loaded")  # this should show up
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": URLRouter(
-        camera.routing.websocket_urlpatterns
+        camera_stream.routing.websocket_urlpatterns
     ),
 })
