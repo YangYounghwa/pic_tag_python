263d6a1c194fc003563f021c70382b3a0b50d475
YangYounghwa <51239748+YangYounghwa@users.noreply.github.com>
2025-07-27 19:29:59 +0900

    pyturboJPEG testing readme 수정

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("monitoring.urls")),
+    path('mjpeg/', include("camera_stream.urls")), 
     path("admin/", admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
