f66e7d0c9afa1b56571826d28f5de0ae5913fa9c
YangYounghwa <yh9369@gmail.com>
2025-07-29 18:44:05 +0900

    changes on cropper to save img in jpg instead of png.

--- pic_tag/camera_worker/grouper/__init__.py
+++ pic_tag/camera_worker/grouper/__init__.py
@@ -1,4 +1,4 @@
 # from .identity_engine import IdentityEngine
 from .id_loggerv2 import IdentityLogger
 # from .identity_enginev2 import IdentityEngine
-from .identity_enginev3 import IdentityEngine
+from .identity_enginev4 import IdentityEngine
