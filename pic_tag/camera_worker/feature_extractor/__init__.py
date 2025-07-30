1b956accf912b7a5dc23e27bbec7be2efc98d053
YangYounghwa <yh9369@gmail.com>
2025-07-30 14:27:48 +0900

    optimization removed just for now

--- pic_tag/camera_worker/feature_extractor/__init__.py
+++ pic_tag/camera_worker/feature_extractor/__init__.py
@@ -1,4 +1,4 @@
-from .feature_extractorv2 import extract_features
+from .feature_extractor import extract_features
 from .ReID_modelv2 import YOLOv11ReID
 from .resizePad import ResizePad
 from .ReID_attenv2 import ReIDAtten_v2
