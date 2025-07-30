7b08f96ba9a51fcea97d92786605afef11e732d2
YangYounghwa <yh9369@gmail.com>
2025-07-30 16:19:24 +0900

    Minor change

--- pic_tag/camera_worker/cropper/__init__.py
+++ pic_tag/camera_worker/cropper/__init__.py
@@ -1,3 +1,9 @@
-from .cam_cropper import capture_frames
+import platform
+
+if "intel" in platform.processor().lower():
+    import cam_cropperv2 as cam_cropper
+else:
+    import cam_cropper as cam_cropper
+    
 from .video_cropper import capture_video_frames
 from .cropper_utils import model_load, make_folder, draw_bounding_box
--- pic_tag/camera_worker/feature_extractor/__init__.py
+++ pic_tag/camera_worker/feature_extractor/__init__.py
@@ -1,4 +1,13 @@
-from .feature_extractor import extract_features
+import platform
+
+if "intel" in platform.processor().lower():
+    from .feature_extractorv2 import extract_features
+else:
+    from .feature_extractor import extract_features
+
+
+
+
 from .ReID_modelv2 import YOLOv11ReID
 from .resizePad import ResizePad
 from .ReID_attenv2 import ReIDAtten_v2
